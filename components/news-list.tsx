01b8de94f3589c5ceec43c92ecabfa6f48efe16d
HaiDT <dotrihai1999@gmail.com>
2025-12-17 23:51:58 +0700

    catch-all fallback routes

---
+++ components/news-list.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
+type News = {
+  id: string;
+  slug: string;
+  title: string;
+  image: string;
+  date: string;
+  content: string;
+};
+
+export default function NewsList({ news }: { news: News[] }) {
+  return (
+    <ul className="news-list">
+      {news.map((newsItem) => (
+        <li key={newsItem.id} className="news-item">
+          <Link href={`/news/${newsItem.slug}`}>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+
+            <span>{newsItem.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
