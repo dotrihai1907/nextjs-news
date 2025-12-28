638a0d1b6c000ed72a31d811828ed1f0cab5d510
HaiDT <dotrihai1999@gmail.com>
2025-12-28 21:40:25 +0700

    feat: nested routes inside dynamic routes

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { use } from "react";
 
@@ -13,7 +14,9 @@ export default function NewsDetailPage(props: PageProps<"/news/[slug]">) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
