eb23e5e9e9dd702718abc521f6ca05d887fda2bd
HaiDT <dotrihai1999@gmail.com>
2026-01-10 23:33:57 +0700

    feat: option client-side rendering

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,39 @@
+"use client";
+
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news data.");
+        setIsLoading(false);
+      }
+
+      const data = await response.json();
+      setNews(data);
+      setIsLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
